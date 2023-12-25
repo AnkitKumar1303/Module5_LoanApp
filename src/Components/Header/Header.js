81da9d49a7c2cf7aa5d5b3cf594a51bc7975c8ee
AnkitKumar1303 <ankitjaiswalnick@gmail.com>
2023-12-25 23:53:03 +0530

    Loan Done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css'
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="nav">
+        <a href="/">Loan App</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
